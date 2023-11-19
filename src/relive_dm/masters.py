7673644747d22d8180fcbc463fb1cf6b08472611
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2023-11-19 12:13:23 -0800

    Read luac files for merging rather than json

--- src/relive_dm/masters.py
+++ src/relive_dm/masters.py
@@ -3,6 +3,8 @@ import logging
 from pathlib import Path
 from typing import Any, TypeAlias
 
+from relive_dm.lua import read_lua_table
+
 logger = logging.getLogger(__name__)
 
 MasterDict: TypeAlias = dict[int, dict[str, Any]]
@@ -28,27 +30,27 @@ def merge_masters(primary: MasterDict, other: MasterDict) -> MasterDict:
     return primary
 
 
-def correct_arrays(data: MasterDict) -> MasterDict:
+def convert_dicts_to_lists(data: MasterDict) -> MasterDict:
+    """Detects dictionaries that should be lists and converts them."""
     keys = set()
-    for v in data.values():
-        if not isinstance(v, dict):
+    for sv in data.values():
+        if not isinstance(sv, dict):
             return data
-        keys |= set(v.keys())
-    for key in keys:
-        values = [v[key] for v in data.values() if key in v]
-        is_array = True
-        for v in values:
-            if not (isinstance(v, dict) and all(k.isdigit() for k in v.keys())):
-                is_array = False
+        keys |= set(sv.keys())
+    for sub_key in keys:
+        sub_values = [v[sub_key] for v in data.values() if sub_key in v]
+        for sv in sub_values:
+            if not (
+                isinstance(sv, dict)
+                and all(isinstance(i, int) for i in sv.keys())
+                and min(sv.keys(), default=1) == 1
+                and max(sv.keys(), default=0) == len(sv)
+            ):
                 break
-            indexes = [int(k) for k in v.keys()]
-            if indexes and (min(indexes) != 1 or max(indexes) != len(indexes)):
-                is_array = False
-                break
-        if is_array:
-            for k, v in data.items():
-                if key in v:
-                    v[key] = [v[key][str(i)] for i in range(1, len(v[key]) + 1)]
+        else:  # no break
+            for v in data.values():
+                if sub_key in v:
+                    v[sub_key] = [v[sub_key][i] for i in range(1, len(v[sub_key]) + 1)]
     return data
 
 
@@ -60,17 +62,15 @@ def merge_all_masters(base_paths: list[Path], out_path: Path):
     if len(base_paths) == 0:
         raise ValueError("Must specify at least one base path.")
     primary, *others = [get_masters_path(base_path) for base_path in base_paths]
-    for master_path in primary.glob("*.json"):
-        data = json.loads(master_path.read_text(encoding="utf-8"))
+    for master_path in primary.glob("*.luac"):
+        data = read_lua_table(master_path.read_bytes())
         for other in others:
             other_path = other / master_path.name
             if other_path.exists():
-                data = merge_masters(
-                    data, json.loads(other_path.read_text(encoding="utf-8"))
-                )
-        data = correct_arrays(data)
+                data = merge_masters(data, read_lua_table(other_path.read_bytes()))
+        data = convert_dicts_to_lists(data)
         out_path.mkdir(parents=True, exist_ok=True)
-        (out_path / master_path.name).write_text(
+        (out_path / f"{master_path.stem}.json").write_text(
             json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True),
             encoding="utf-8",
         )
